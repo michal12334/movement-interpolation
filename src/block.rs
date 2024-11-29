509dcaad42a560851d726cabf8a3bf03e1424e8c
Michal Boguszewski <michal1842001@gmail.com>
2024-11-29 23:33:26 +0100

    Add x and y arrows

--- src/block.rs
+++ src/block.rs
@@ -4,6 +4,7 @@ use derive_getters::Getters;
 use glium::glutin::surface::WindowSurface;
 use glium::index::PrimitiveType;
 use glium::{Display, IndexBuffer, VertexBuffer};
+use nalgebra::{Rotation3, Vector3};
 
 use crate::vertex::Vertex;
 
@@ -15,8 +16,8 @@ pub struct Block {
 
 impl Block {
     pub fn generate(divisions_count: u16, display: &Display<WindowSurface>) -> Self {
-        let mut vertices = Vec::new();
-        let mut indices = Vec::new();
+        let mut z_vertices = Vec::new();
+        let mut z_indices = Vec::new();
         let radius = 1f32;
         let len = 5f32;
         for i in 0..divisions_count {
@@ -26,13 +27,13 @@ impl Block {
             let normal = [0f32, 0f32, 1f32];
             let position = [radius * x, radius * y, 0f32];
             let color = [0f32, 0f32, 1f32];
-            vertices.push(Vertex::new(position, normal, color));
+            z_vertices.push(Vertex::new(position, normal, color));
 
-            indices.push(i);
-            indices.push((i + 1) % divisions_count);
-            indices.push(divisions_count);
+            z_indices.push(i);
+            z_indices.push((i + 1) % divisions_count);
+            z_indices.push(divisions_count);
         }
-        vertices.push(Vertex::new(
+        z_vertices.push(Vertex::new(
             [0f32, 0f32, 0f32],
             [0f32, 0f32, 1f32],
             [0f32, 0f32, 1f32],
@@ -45,15 +46,15 @@ impl Block {
             let normal = [x, y, 0f32];
             let position = [radius * x, radius * y, 0f32];
             let color = [0f32, 0f32, 1f32];
-            vertices.push(Vertex::new(position, normal, color));
+            z_vertices.push(Vertex::new(position, normal, color));
 
-            indices.push(divisions_count + 1 + i);
-            indices.push(2 * divisions_count + 1 + i);
-            indices.push(divisions_count + 1 + (i + 1) % divisions_count);
+            z_indices.push(divisions_count + 1 + i);
+            z_indices.push(2 * divisions_count + 1 + i);
+            z_indices.push(divisions_count + 1 + (i + 1) % divisions_count);
 
-            indices.push(divisions_count + 1 + (i + 1) % divisions_count);
-            indices.push(2 * divisions_count + 1 + i);
-            indices.push(2 * divisions_count + 1 + (i + 1) % divisions_count);
+            z_indices.push(divisions_count + 1 + (i + 1) % divisions_count);
+            z_indices.push(2 * divisions_count + 1 + i);
+            z_indices.push(2 * divisions_count + 1 + (i + 1) % divisions_count);
         }
 
         for i in 0..divisions_count {
@@ -64,21 +65,69 @@ impl Block {
             let normal = [x, y, 0f32];
             let position = [radius * x, radius * y, z];
             let color = [0f32, 0f32, 1f32];
-            vertices.push(Vertex::new(position, normal, color));
+            z_vertices.push(Vertex::new(position, normal, color));
 
-            indices.push(2 * divisions_count + 1 + i);
-            indices.push(3 * divisions_count + 1);
-            indices.push(2 * divisions_count + 1 + (i + 1) % divisions_count);
+            z_indices.push(2 * divisions_count + 1 + i);
+            z_indices.push(3 * divisions_count + 1);
+            z_indices.push(2 * divisions_count + 1 + (i + 1) % divisions_count);
         }
-        vertices.push(Vertex::new(
+        z_vertices.push(Vertex::new(
             [0f32, 0f32, -len - radius],
             [0f32, 0f32, 1f32],
             [0f32, 0f32, 1f32],
         ));
 
+        let x_vertices = z_vertices
+            .iter()
+            .map(|v| {
+                let r = Rotation3::from_euler_angles(0f32, PI / 2f32, 0f32);
+                let p = Vector3::new(v.position()[0], v.position()[1], v.position()[2]);
+                let n = Vector3::new(v.normal()[0], v.normal()[1], v.normal()[2]);
+
+                let p = r * p;
+                let n = r * n;
+
+                let c = [1f32, 0f32, 0f32];
+
+                Vertex::new(p.data.0[0], n.data.0[0], c)
+            })
+            .collect::<Vec<_>>();
+
+        let x_indices = z_indices
+            .iter()
+            .map(|i| i + z_vertices.len() as u16)
+            .collect::<Vec<_>>();
+
+        let y_vertices = z_vertices
+            .iter()
+            .map(|v| {
+                let r = Rotation3::from_euler_angles(PI / 2f32, 0f32, 0f32);
+                let p = Vector3::new(v.position()[0], v.position()[1], v.position()[2]);
+                let n = Vector3::new(v.normal()[0], v.normal()[1], v.normal()[2]);
+
+                let p = r * p;
+                let n = r * n;
+
+                let c = [0f32, 1f32, 0f32];
+
+                Vertex::new(p.data.0[0], n.data.0[0], c)
+            })
+            .collect::<Vec<_>>();
+
+        let y_indices = z_indices
+            .iter()
+            .map(|i| i + 2 * z_vertices.len() as u16)
+            .collect::<Vec<_>>();
+
         Self {
-            vertices: VertexBuffer::new(display, &vertices).unwrap(),
-            indices: IndexBuffer::new(display, PrimitiveType::TrianglesList, &indices).unwrap(),
+            vertices: VertexBuffer::new(display, &[z_vertices, x_vertices, y_vertices].concat())
+                .unwrap(),
+            indices: IndexBuffer::new(
+                display,
+                PrimitiveType::TrianglesList,
+                &[z_indices, x_indices, y_indices].concat(),
+            )
+            .unwrap(),
         }
     }
 }
